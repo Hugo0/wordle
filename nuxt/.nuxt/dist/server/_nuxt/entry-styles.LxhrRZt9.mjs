0dc97fa9c740942891a80d554ab50d96c26f5c0e
Hugo Montenegro <h@hugo0.com>
2026-03-14 20:54:52 +0000

    fix: add nuxt .gitignore, remove build artifacts from tracking

--- nuxt/.nuxt/dist/server/_nuxt/entry-styles.LxhrRZt9.mjs
+++
@@ -1,4 +0,0 @@
-import style_0 from "./entry-styles-1.mjs-SvhyC6Db.js";
-export default [
-  style_0
-]
--- nuxt/.output/server/chunks/build/styles.mjs
+++
@@ -1,7 +0,0 @@
-const interopDefault = r => r.default || r || [];
-const styles = {
-  "node_modules/.pnpm/nuxt@3.21.2_@parcel+watcher@2.5.6_@types+node@25.5.0_@vue+compiler-sfc@3.5.30_cac@6.7.1_6d7d18c5e2216e9bd85e418e561ff949/node_modules/nuxt/dist/app/entry.js": () => import('./entry-styles.LxhrRZt9.mjs').then(interopDefault)
-};
-
-export { styles as default };
-//# sourceMappingURL=styles.mjs.map
